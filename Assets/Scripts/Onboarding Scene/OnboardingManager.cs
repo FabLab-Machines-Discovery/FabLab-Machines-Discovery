3fec08eb12eaeb964bcb2c371c5e4f0154c1b5cb
Aymen Hammami <hammami.aym@outlook.com>
2023-03-03 15:21:16 +0100

    improvements to Onboarding Scene and added Scene Loader utility class

--- Assets/Scripts/Onboarding Scene/OnboardingManager.cs
+++ Assets/Scripts/Onboarding Scene/OnboardingManager.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using UnityEngine;
+using Utility;
 
 namespace Onboarding_Scene
 {
-    public class PanelManager : MonoBehaviour
+    public class OnboardingManager : MonoBehaviour
     {
         public GameObject logoPanel;
         [Tooltip("Time to wait in seconds before switching from logo panel to start panel")]
@@ -11,6 +12,7 @@ namespace Onboarding_Scene
         public GameObject startPanel;
         public GameObject tutorialPanel;
         public GameObject panelPagination;
+        public string vuforiaSceneName;
 
         private void OnValidate()
         {
@@ -34,6 +36,11 @@ namespace Onboarding_Scene
             startPanel.SetActive(false);
             tutorialPanel.SetActive(true);
             panelPagination.SetActive(true);
+        }
+
+        public void LoadVuforiaScene()
+        {
+            SceneLoader.LoadScene(vuforiaSceneName);
         }
     }
     

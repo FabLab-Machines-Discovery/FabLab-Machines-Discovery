2aa5504d81ff7ba010ccd9e7e2944e57627abf11
Aymen Hammami <hammami.aym@outlook.com>
2023-03-08 14:19:13 +0100

    new scene for popup ui and testing

--- Assets/Scripts/Vuforia/VuforiaInitializer.cs
+++ Assets/Scripts/Vuforia/VuforiaInitializer.cs
@@ -6,9 +6,21 @@ namespace Vuforia
     [DefaultExecutionOrder(-1)]
     public class VuforiaInitializer : MonoBehaviour
     {
+        public GameObject loadingPanel;
         private void Awake()
         {
             VuforiaApplication.Instance.Initialize();
+            VuforiaApplication.Instance.OnVuforiaStarted += DisableLoadingScreen;
+        }
+
+        private void OnDisable()
+        {
+            VuforiaApplication.Instance.OnVuforiaStarted -= DisableLoadingScreen;
+        }
+
+        private void DisableLoadingScreen()
+        {
+            loadingPanel.SetActive(false);
         }
     }
 }

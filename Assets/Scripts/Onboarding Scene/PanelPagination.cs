16178f9735b4bebc3e97d43684ca9099870f64ec
Aymen Hammami <hammami.aym@outlook.com>
2023-03-10 14:45:12 +0100

    moved observing OnSwipe event from UIAnimation into PanelSwipeable component

--- Assets/Scripts/Onboarding Scene/PanelPagination.cs
+++ Assets/Scripts/Onboarding Scene/PanelPagination.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,26 +5,41 @@ namespace Onboarding_Scene
 {
     public class PanelPagination : MonoBehaviour
     {
+        [Tooltip("The PanelSwiper component that the pagination is applying to")]
+        public PanelSwiper panelSwiper;
+        
+        [Tooltip("Color applied for the current/active panel")]
         public Color activeColor;
+        
+        [Tooltip("Color applied for the other panels")]
         public Color inactiveColor;
         
+        // List of dots that represent the panels respectively and are used for pagination
         private Image[] _dots;
+        
         private void Start()
         {
             _dots = GetComponentsInChildren<Image>();
-            PanelSwiper.OnSwipe += SetPagination;
+            // Subscribe to the OnSwipe event
+            panelSwiper.OnSwipe += UpdatePagination;
         }
 
         private void OnDisable()
         {
-            PanelSwiper.OnSwipe -= SetPagination;
+            // Unsubscribe of the OnSwipe event
+            panelSwiper.OnSwipe -= UpdatePagination;
         }
-
-        private void SetPagination(int panelIndex)
+        
+        /*
+         * Updates the color of the dots that represent the panels respectively.
+         * The dot that represents the active panel will have the activeColor applied to it.
+         * The dots that represent the other panels will have the inactiveColor applied to them.
+         */
+        private void UpdatePagination(int activePanelIndex)
         {
             for (int i = 0; i < _dots.Length; i++)
             {
-                _dots[i].color = i == panelIndex ? activeColor : inactiveColor;
+                _dots[i].color = i == activePanelIndex ? activeColor : inactiveColor;
             }
         }
     }
